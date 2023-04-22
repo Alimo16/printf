482380dc32421b4532d42c2dd154f75e3ddd15ee
Alimo16 <alimohamedmoniem@gmail.com>
2023-04-22 05:02:22 -0700

    merge

--- print_functions.c
+++ print_functions.c
@@ -1,4 +1,4 @@
-#include "main."
+#include "main.h"
 
 /* Print a  Char */
 
@@ -19,4 +19,33 @@ int print_char(va_list types, char buffer[], int flags,
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+
+/* Print a string */
+
+/**
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+
+int print_string(va_list types, char buffer[], int flags,
+		int width, int precision, int size)
+{
+	int length = 0, i;
+	char *str = va_arg(types, char *);
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+	{
+
 
