97941359aa8f0595329d69f8f4d87c647618ce92
Alimo16 <alimohamedmoniem@gmail.com>
2023-04-23 02:14:29 -0700

    main.h

--- main.h
+++ main.h
@@ -27,7 +27,7 @@ int _print(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[],
                  int flags, int width, int precision, int size);
 
-int print_char(va_list types, char buffer[], int flags, int width, 
+int print_char(va_list types, char buffer[], int flags, int width,
                int precision, int size);
 int print_string(va_list types, char buffer[], int flags,
                  int width, int precision, int size);
